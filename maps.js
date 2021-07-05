f845b19df2d989b0484e4c912ffe8509f7c86587
Reynaldo Villar Garavini <67124541+ReynaldoGaravini@users.noreply.github.com>
2021-07-05 19:29:26 -0300

    Sprint final

--- maps.js
+++ maps.js
@@ -9,18 +9,18 @@ function showPosition(position) {
 
     // Configurações do mapa
     var options = {
-        zoom:16,
-        center:{lat:x,lng:y}
+        zoom: 16,
+        center: { lat: x, lng: y }
     }
-  
+
     // Novo mapa
     var map = new google.maps.Map(document.getElementById('map'), options);
 
     // Link para o Google Maps com os pontos de reciclagem mais proximos
     let z = document.getElementById('link-google-maps');
-    z.innerHTML = `<a class="btn btn-primary botao" href="https://www.google.com/maps/search/?api=1&query=pontos+proximos+de+reciclagem">Achar Pontos Perto de Mim</a>`
+    z.innerHTML = `<a class="btn" href="https://www.google.com/maps/search/?api=1&query=pontos+proximos+de+reciclagem"><img src="img/ponto-no-mapa.png" alt="ponto-no-mapa" style="max-width: 60px;"></a>`
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     getLocation();
 })
